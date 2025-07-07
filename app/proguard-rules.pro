zgfgNew/PlayIntegrityFork

Refine proguard rules further
Thanks @TheFreeman193!

app/proguard-rules.pro
@@ -1,5 +1,5 @@
 -keep class es.chiteroman.playintegrityfix.EntryPoint { public <methods>; }
 -keep class es.chiteroman.playintegrityfix.EntryPointVending { public <methods>; }
 -keepclassmembers class es.chiteroman.playintegrityfix.CustomKeyStoreSpi
--keepclassmembers class es.chiteroman.playintegrityfix.CustomPackageInfoCreator { public <methods>; }
+-keepclassmembers class es.chiteroman.playintegrityfix.CustomPackageInfoCreator { public <init>(...); }
 -keepclassmembers class es.chiteroman.playintegrityfix.CustomProvider

AmirulAndalib/GPhotosUnlimited

app/proguard-rules.pro
@@ -1,5 +1,5 @@
 -keep class com.rev4n.unlimitedphotos.EntryPoint { public <methods>; }
 -keep class com.rev4n.unlimitedphotos.EntryPointVending { public <methods>; }
 -keepclassmembers class com.rev4n.unlimitedphotos.CustomKeyStoreSpi
--keepclassmembers class com.rev4n.unlimitedphotos.CustomPackageInfoCreator { public <methods>; }
+-keepclassmembers class com.rev4n.unlimitedphotos.CustomPackageInfoCreator { public <init>(...); }
 -keepclassmembers class com.rev4n.unlimitedphotos.CustomProvider