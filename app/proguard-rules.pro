zgfgNew/PlayIntegrityFork

Fix proguard rules for AGP 8.9.0+
- CustomPackageInfoCreator public methods must now be explicitly kept to avoid spoofSignature crashes documented in https://github.com/osm0sis/PlayIntegrityFork/commit/2cdee60781d5b24cec9baadaf1a2489318a64751

app/proguard-rules.pro
@@ -1,5 +1,5 @@
--keep class es.chiteroman.playintegrityfix.EntryPoint {public <methods>;}
--keep class es.chiteroman.playintegrityfix.EntryPointVending {public <methods>;}
--keep class es.chiteroman.playintegrityfix.CustomProvider
--keep class es.chiteroman.playintegrityfix.CustomKeyStoreSpi
--keep class es.chiteroman.playintegrityfix.CustomPackageInfoCreator
+-keep class es.chiteroman.playintegrityfix.EntryPoint { public <methods>; }
+-keep class es.chiteroman.playintegrityfix.EntryPointVending { public <methods>; }
+-keepclassmembers class es.chiteroman.playintegrityfix.CustomKeyStoreSpi
+-keepclassmembers class es.chiteroman.playintegrityfix.CustomPackageInfoCreator { public <methods>; }
+-keepclassmembers class es.chiteroman.playintegrityfix.CustomProvider

AmirulAndalib/GPhotosUnlimited

app/proguard-rules.pro
@@ -1,4 +1,5 @@
--keep class com.rev4n.unlimitedphotos.EntryPoint {public <methods>;}
--keep class com.rev4n.unlimitedphotos.CustomProvider
--keep class com.rev4n.unlimitedphotos.CustomKeyStoreSpi
--keep class com.rev4n.unlimitedphotos.CustomPackageInfoCreator
+-keep class com.rev4n.unlimitedphotos.EntryPoint { public <methods>; }
+-keep class com.rev4n.unlimitedphotos.EntryPointVending { public <methods>; }
+-keepclassmembers class com.rev4n.unlimitedphotos.CustomKeyStoreSpi
+-keepclassmembers class com.rev4n.unlimitedphotos.CustomPackageInfoCreator { public <methods>; }
+-keepclassmembers class com.rev4n.unlimitedphotos.CustomProvider