osm0sis/PlayIntegrityFork

Remove special case for logFields, simplify logs
- Run logFields only when spoofProvider, spoofBuild, or spoofSignature is on
- Simplify log outputs; PS -> Play Store, DG -> DroidGuard

app/src/main/java/es/chiteroman/playintegrityfix/EntryPointVending.java
@@ -8,7 +8,7 @@ import android.util.Log;
 public final class EntryPointVending {
 
     private static void LOG(String msg) {
-        Log.d("PIF/Java:vending", msg);
+        Log.d("PIF/Java:PS", msg);
     }
 
     @SuppressLint("DefaultLocale")

AmirulAndalib/GPhotosUnlimited

app/src/main/java/com/rev4n/unlimitedphotos/EntryPointVending.java
@@ -8,7 +8,7 @@ import android.util.Log;
 public final class EntryPointVending {
 
     private static void LOG(String msg) {
-        Log.d("FGP/Java:vending", msg);
+        Log.d("FGP/Java:PS", msg);
     }
 
     @SuppressLint("DefaultLocale")